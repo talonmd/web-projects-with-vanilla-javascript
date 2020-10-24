1dda4320a9911d35ef785c34cfbad5b8af17eccc
Talon Durrant <talonmdurrant@gmail.com>
2020-10-23 22:20:50 -0600

    finished project 8 meal finder

--- 8__meal-finder/script.js
+++ 8__meal-finder/script.js
@@ -21,23 +21,25 @@ function searchMeal(e) {
       .then((res) => res.json())
       .then((data) => {
         console.log(data)
-        resultHeading.innerHTML = `<h2>Search results for "${term}"</h2>`
+        resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`
 
-        if (data.meals == null) {
-          resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`
         } else {
           mealsEl.innerHTML = data.meals
             .map(
-              (meal) => `<div class="meal"><img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-          <div class="meal-info" data-mealID="${meal.idMeal}">
-          <h3>${meal.strMeal}</h3>
-          </div>
-          </div>`
+              (meal) => `
+            <div class="meal">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+              <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+              </div>
+            </div>
+          `
             )
             .join("")
         }
       })
-
     // Clear search text
     search.value = ""
   } else {
@@ -46,8 +48,23 @@ function searchMeal(e) {
 }
 
 // Fetch meal by ID
-function getMealByID(mealID) {
+function getMealById(mealID) {
   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0]
+
+      addMealToDOM(meal)
+    })
+}
+
+// Fetch random meal from API
+function getRandomMeal() {
+  // Clear meals and heading
+  mealsEl.innerHTML = ""
+  resultHeading.innerHTML = ""
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
     .then((res) => res.json())
     .then((data) => {
       const meal = data.meals[0]
@@ -61,31 +78,34 @@ function addMealToDOM(meal) {
 
   for (let i = 1; i <= 20; i++) {
     if (meal[`strIngredient${i}`]) {
-      ingredients.push(`${meal[`strIngredient${i} - ${meal[`strMeasure${i}`]}`]}`)
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
     } else {
       break
     }
   }
+
   single_mealEl.innerHTML = `
   <div class="single-meal">
-  <h1>${meal.strMeal}</h1>
-  <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-  <div class="single-meal-info">
-  ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
-  ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+    <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+    <div class="single-meal-info">
+      ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+      ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+    </div>
+    <div class="main">
+      <p>${meal.strInstructions}</p>
+      <h2>Ingredients</h2>
+      <ul>
+      ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+      </ul>
+    </div>
   </div>
-  <div class="main">
-  <p>${meal.strInstructions}</p>
-  <h2>Ingredients</h2>
-  <ul>
-    ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
-  </ul>
-  </div>
-  </div>`
+`
 }
 
 // Event listeners
 submit.addEventListener("submit", searchMeal)
+random.addEventListener("click", getRandomMeal)
 
 mealsEl.addEventListener("click", (e) => {
   const mealInfo = e.path.find((item) => {
@@ -98,6 +118,6 @@ mealsEl.addEventListener("click", (e) => {
 
   if (mealInfo) {
     const mealID = mealInfo.getAttribute("data-mealid")
-    getMealByID(mealID)
+    getMealById(mealID)
   }
 })
